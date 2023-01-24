bfd9a1a1c2bf7fd0079b1647f544cca1d436ad68
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2023-01-24 12:57:38 -0800

    generalization after moving to fuoco

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# General used
+
+mkdir config_files_cookie_monster
+
+.
+├── complete
+├── pending
+├── stop_training
+├── template.yaml
+└── training
+
+drwxrwxr-x 2 hpinkard_waller henry 4096 Jan 22 19:58 complete
+drwxrwxr-x 2 hpinkard_waller henry 4096 Jan 22 20:01 pending
+drwxrwxrwx 2 hpinkard_waller henry 4096 Nov 11 10:04 stop_training
+-rw-rw-r-- 1 hpinkard_waller henry  862 Jan 23 17:55 template.yaml
+drwxrwxr-x 2 hpinkard_waller henry 4096 Jan 22 20:01 training
+
+
+# launch
+python /home/hpinkard_waller/GitRepos/cookie_monster/launch_backend.py "/home/hpinkard_waller/GitRepos/microscoBayes/deep_density/train_model.py" "/home/hpinkard_waller/config_files_cookie_monster/" "/home/hpinkard_waller/models/"
+
+
+# For use with jupyterlab
+
 Add to ~/.jupyter
 
 So that the checkpoints don't save in directory with yaml files
--- launch_backend.py
+++ launch_backend.py
@@ -1,7 +1,6 @@
 # !pip install nvidia-ml-py3
 
 import threading
-import zmq
 import nvidia_smi
 import numpy as np
 import time
@@ -10,16 +9,20 @@ import os
 import shutil
 import argparse
 
+DEBUG = False
+
 parser = argparse.ArgumentParser()
 # Required arguments
-parser.add_argument('train_script_path', help="GPU_index", type=str)
+parser.add_argument('train_script_path', help="train script to use", type=str)
+parser.add_argument('status_dir', help="where config files kept", type=str)
+parser.add_argument('save_dir', help="where trained models and data saved", type=str)
 args = parser.parse_args()
 train_script_path = args.train_script_path
 
 
 # make root directory for model and logging
-STATUS_DIR = "/home/hpinkard_waller/cookie_monster/"
-SAVING_DIR_ROOT = "/home/hpinkard_waller/2tb_ssd/models/"
+STATUS_DIR = args.status_dir
+SAVING_DIR_ROOT = args.save_dir
 
 
 USAGE_THRESHOLD = 30 # need less than 30 percent usage
@@ -39,21 +42,47 @@ def check_GPU_status(index):
     return free_GB, gpu_usage
 
 
-def launch_training(gpu_index, saving_dir, config_file_path, debug=False):
+def launch_training(gpu_index, saving_dir, config_file_path, stdout_path, debug=DEBUG):
     """
     Commence training of new model
     """
     if not os.path.exists(config_file_path):
         raise Exception("Config file doesnt exist", config_file_path)
-    p = subprocess.Popen(["ipython", train_script_path, str(gpu_index), str(saving_dir), config_file_path],
-                          stdout=subprocess.PIPE)        
+    process = subprocess.Popen(["ipython", train_script_path, str(gpu_index), str(saving_dir), config_file_path],
+                          stdout=subprocess.PIPE)     
+    
+    
+    # launch a thread for monitoring 
+
+    def read_process_output():
+        # Read the stdout line by line
+        
+        
+        with open(stdout_path + '/process_output.txt', 'a') as f:
+            while True:
+                line = process.stdout.readline()
+                if not line:
+                    # The process has exited, so shut down the thread
+                    break       
+                f.write(line.decode())
+                f.flush()
+                # Check if the process has exited (still needed?)
+                exit_code = process.poll()
+                if exit_code is not None:
+                    # The process has exited
+                    print('Process has exited with code', exit_code)  
+                    return
+
+    # Create and start the new thread
+    thread = threading.Thread(target=read_process_output)
+    thread.start()
 
     if debug:
         while True:
-            line = p.stdout.readline()
+            line = process.stdout.readline()
             print(line)
             if not line: break
-    return p
+    return process
 
 
 def create_saving_dir(saving_dir_root, model_name, overwrite):
@@ -64,7 +93,7 @@ def create_saving_dir(saving_dir_root, model_name, overwrite):
             shutil.rmtree(dest)
     else:
         if os.path.exists(dest):
-            raise Exception('saving dir already exists')
+            raise Exception('saving dir already exists: ' + dest)
     os.mkdir(dest)
     return dest   
             
@@ -77,7 +106,6 @@ def check_for_kill_flag(active_training_processes, gpu_indices, gpu_usage, gpu_m
         if len(gpu_index) == 0:
             return gpu_usage, gpu_memory
         f.truncate(0) #clear file
-    # os.remove(STATUS_DIR + '/stop_training/stop')
     # kill a process
     if len(active_training_processes) > 0:
         print("killing process")
@@ -110,40 +138,18 @@ gpu_memory = {index: [] for index in range(num_GPUs)}
 while True:
     start_time = time.time()
     # monitor for available GPUs
-    available_GPUs = []
-    for index in range(num_GPUs):
-        # record usage
-        memory, usage = check_GPU_status(index)
-        usage_history = gpu_usage[index]
-        memory_history = gpu_memory[index]
-        usage_history.append(usage)
-        memory_history.append(memory)
-        # compute averages
-        usage_history = usage_history[1:]
-        memory_history = memory_history[1:]
-        average_usage = np.mean(usage_history)
-        average_memory = np.mean(memory_history)
-        if average_usage < USAGE_THRESHOLD and usage < USAGE_THRESHOLD and \
-            memory > MEMORY_THRESHOLD and average_memory > MEMORY_THRESHOLD:
-            available_GPUs.append(index)
-    for gpu_index in gpu_indices.values():
-        if gpu_index in available_GPUs:
-            available_GPUs.remove(gpu_index)
-    
-    if len(memory_history) < num_gpu_samples and len(available_GPUs) < 2:
-        print("waiting {:.2f}%".format(len(memory_history) / num_gpu_samples *100), '\r', end='')
-        time.sleep(gpu_status_delta_t)
-        continue
-    
+
+            
     # clear completed processes
     for config_file in list(active_training_processes.keys()):
         process = active_training_processes[config_file]
         if process.poll() is not None:
+            print('clearing process')
             # its finished
             del active_training_processes[config_file]
             del gpu_indices[config_file]
 
-    
+
     # update config files
     configs_to_retry = []
     for config_file_name in os.listdir(STATUS_DIR + 'training'):
@@ -154,8 +160,51 @@ while True:
                 shutil.move(STATUS_DIR + 'training/' + config_file_name, STATUS_DIR + 'complete/' + config_file_name)
             else:
                 configs_to_retry.append(config_file_name)
+
+                
+    #check for kill flag
+    gpu_usage, gpu_memory = check_for_kill_flag(active_training_processes, gpu_indices, gpu_usage, gpu_memory)
+            
+                
+    available_GPUs = []
+    for index in range(num_GPUs):
+          
+        # record usage
+        memory, usage = check_GPU_status(index)
+        gpu_usage[index].append(usage)
+        gpu_memory[index].append(memory)
+        if len(gpu_usage[index]) > num_gpu_samples:
+            gpu_usage[index] = gpu_usage[index][-num_gpu_samples:]
+            gpu_memory[index] = gpu_memory[index][-num_gpu_samples:]
+        # compute averages
+        average_usage = np.mean(gpu_usage[index])
+        average_memory = np.mean(gpu_memory[index])
+        if average_usage < USAGE_THRESHOLD and usage < USAGE_THRESHOLD and \
+            memory > MEMORY_THRESHOLD and average_memory > MEMORY_THRESHOLD:
+            available_GPUs.append(index)
+        else:
+            print('GPU ', index, ' unavailable')
+    for gpu_index in gpu_indices.values():
+        if gpu_index in available_GPUs:
+            available_GPUs.remove(gpu_index)
+    
+    if len(available_GPUs) == 0:
+        print('No GPUs available')
+        time.sleep(gpu_status_delta_t)
+        continue
+
+
+    
+    while True:
+        if len(available_GPUs) == 0: 
+            print('no GPUs available')
+            break
         
-    for gpu_index in available_GPUs:
+        if len(available_GPUs) == 1 and len(gpu_usage[available_GPUs[0]]) < num_gpu_samples:
+            print("waiting before hogging last GPU{:.2f}%".format(len(gpu_usage[available_GPUs[0]]) / num_gpu_samples *100))
+            break
+        
+        print('available gpus', available_GPUs)
         # anything to train?
         #    Check for failed attempts
         #    Check for pending config files
@@ -166,20 +215,25 @@ while True:
         elif len(pending_configs) > 0:
             config_file_name = pending_configs.pop(0)
             saving_dir = create_saving_dir(SAVING_DIR_ROOT, config_file_name[:-5], overwrite=False)
+            print('moving pending config file to training')
             shutil.move(STATUS_DIR + 'pending/' + config_file_name, STATUS_DIR + 'training/' + config_file_name)
         else:
+            print('waiting for something to train')
             break #nothing to train
         
+        # prefer GPUs with most available memory:
+        free_mem = np.array([np.mean(gpu_memory[index]) for index in available_GPUs])
+        to_use = np.argmax(free_mem)
+        print('next to use ', available_GPUs[to_use])
+        gpu_index = available_GPUs.pop(to_use)
         config_file_path = STATUS_DIR + 'training/' + config_file_name
-        active_training_processes[config_file_name] = launch_training(gpu_index, saving_dir, config_file_path)
+        active_training_processes[config_file_name] = launch_training(gpu_index, saving_dir, config_file_path, saving_dir)
         gpu_indices[config_file_name] = gpu_index
         print("Launching training {} on GPU {}".format(config_file_path, gpu_index))
 
-    #check for kill flag
-    gpu_usage, gpu_memory = check_for_kill_flag(active_training_processes, gpu_indices, gpu_usage, gpu_memory)
-            
+
         
     time.sleep(gpu_status_delta_t)
 
-
+print('Exiting loop. This shouldnt happen. Why did this happen')
 # nvidia_smi.nvmlShutdown()
